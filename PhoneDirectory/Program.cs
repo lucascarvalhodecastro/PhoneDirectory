657948ef3f79da83ff57974f456a6f2aeed609e6
Lucas Carvalho de Castro <castro.lucas.eu@gmail.com>
2024-02-08 14:56:22 +0000

    Updated the comments around example CRUD code

--- PhoneDirectory/Program.cs
+++ PhoneDirectory/Program.cs
@@ -15,14 +15,15 @@ namespace PhoneDirectory
             PhoneBookEntry pbe5 = new PhoneBookEntry() { Number = "089-987321", Name = "Mary Maleady", Address = "House 5" };
             PhoneBookEntry pbe6 = new PhoneBookEntry() { Number = "081-112233", Name = "Dermot Boyle", Address = "House 6" };
 
-            /*
+            // Commenting the example pieces
+            /* 
             utils.CreateNewPBE(pbe1);
             utils.CreateNewPBE(pbe2);
             utils.CreateNewPBE(pbe3);
             utils.CreateNewPBE(pbe4);
             utils.CreateNewPBE(pbe5);
             utils.CreateNewPBE(pbe6);
-            */
+            
 
             utils.ReadAllPBE();
             Console.WriteLine("--------------------");
@@ -37,6 +38,7 @@ namespace PhoneDirectory
 
             utils.ReadAllPBE();
             Console.WriteLine("--------------------");
+            */
 
             // The actual lab requirements
 
@@ -60,7 +62,7 @@ namespace PhoneDirectory
             foreach (var record in records)
             {
                 Console.WriteLine($"Number: {record.Number} | Address: {record.Address}");
-            }
+            }          
         }
     }
 }
